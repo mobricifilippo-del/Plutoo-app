8bd305706ab3503b81afa09d59a329cd3d426060
mobricifilippo-del <mobrici.filippo@live.com>
2025-09-18 15:50:14 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,20 @@
-const CACHE_NAME = "plutoo-cache-v1";
+const CACHE_NAME = "plutoo-cache-v23";
 const urlsToCache = [
   "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./dog1.jpg",
-  "./dog2.jpg",
-  "./dog3.jpg",
-  "./dog4.jpg",
+  "./index.html?v=23",
+  "./style.css?v=23",
+  "./app.js?v=23",
+  "./dog1.jpg","./dog2.jpg","./dog3.jpg","./dog4.jpg",
   "./logo-32.jpg"
 ];
 
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
 });
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then((res) => res || fetch(event.request))
   );
 });
 
