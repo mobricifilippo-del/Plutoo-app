ee0519a8b9aca6d91e1f529bba30a766a85731d5
mobricifilippo-del <mobrici.filippo@live.com>
2025-09-18 15:32:45 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,34 @@
-// v1 minimal SW: cache-first per i file statici
-const CACHE = 'plutoo-v1';
-const ASSETS = [
-  './',
-  './index.html',
-  './style.css?v=140',
-  './app.js?v=140',
-  './logo-32.jpg',
-  './dog1.jpg','./dog2.jpg','./dog3.jpg','./dog4.jpg'
+const CACHE_NAME = "plutoo-cache-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./dog1.jpg",
+  "./dog2.jpg",
+  "./dog3.jpg",
+  "./dog4.jpg",
+  "./logo-32.jpg"
 ];
 
-self.addEventListener('install', e=>{
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-  self.skipWaiting();
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
 });
-self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
-  self.clients.claim();
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
-self.addEventListener('fetch', e=>{
-  const req = e.request;
-  e.respondWith(
-    caches.match(req).then(cached => cached || fetch(req))
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
   );
 });
