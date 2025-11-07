a8decc76c8b394b58ae48f4595bf7e7286f99e2f
mobricifilippo-del <mobrici.filippo@live.com>
2025-11-07 12:32:31 +0100

    Create noop-sw.js

---
+++ noop-sw.js
@@ -0,0 +1,18 @@
+// Service Worker disabilitato completamente
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => caches.delete(cacheName))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(fetch(event.request));
+});
