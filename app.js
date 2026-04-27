7025d60be64032e6c22a2a68395596b6835c6339
mobricifilippo-del <mobrici.filippo@live.com>
2026-04-27 13:42:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3757,6 +3757,10 @@ setActiveView("profile");
         <input id="createDogBreed" type="text" value="" placeholder="${state.lang === "it" ? "Razza *" : "Breed *"}" style="background:transparent;border:0;outline:none;color:inherit;width:100%">
       </span>
 
+ <span class="badge create-req" data-req="1" data-label="${state.lang === "it" ? "Zona" : "Area"}" style="padding:.45rem .55rem;flex:1;min-width:42%">
+  <input id="createDogZone" type="text" value="" placeholder="${state.lang === "it" ? "Zona *" : "Area *"}" style="background:transparent;border:0;outline:none;color:inherit;width:100%">
+</span>
+
       <span class="badge create-req" data-req="1" data-label="${state.lang === "it" ? "Età" : "Age"}" style="padding:.45rem .55rem;flex:1;min-width:42%">
         <input id="createDogAge" type="number" min="0" step="1" value="" placeholder="${state.lang === "it" ? "Età *" : "Age *"}" style="background:transparent;border:0;outline:none;color:inherit;width:100%">
       </span>
