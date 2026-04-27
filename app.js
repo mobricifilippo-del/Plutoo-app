ada51ddc3a24685b68184cb4e37a411d6d0ce296
mobricifilippo-del <mobrici.filippo@live.com>
2026-04-27 15:43:14 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3681,6 +3681,14 @@ setActiveView("profile");
   const isCreate = (d && d.isCreate === true) || (d && d.id === "__create__");
   const heroImg = isCreate ? "" : (d.img || "./plutoo-icon-192.png");
 
+  alert(
+  "ZONE CHECK PROFILE\n\n" +
+  "name: " + (d && d.name) + "\n" +
+  "zone: " + (d && d.zone) + "\n" +
+  "km: " + (d && d.km) + "\n" +
+  "id: " + (d && d.id)
+);
+
  profileContent.innerHTML = `
 
   <div class="pp-hero">
