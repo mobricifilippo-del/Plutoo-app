fc68c29fc27c9a8390c270149a2547fde2a9b974
mobricifilippo-del <mobrici.filippo@live.com>
2026-05-06 16:10:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4039,12 +4039,6 @@ setActiveView("profile");
   const ownerDocs = d.ownerDocs || {};
 const dogDocs = d.dogDocs || {};
 
-  alert(
-  "DOGDOCS DEBUG\n" +
-  "dogId: " + (d && d.id) + "\n" +
-  "dogDocs: " + JSON.stringify(dogDocs)
-);
-
   const selfieSrc = d.selfieUrl || "./plutoo-icon-192.png";
 
   const dogStories =
