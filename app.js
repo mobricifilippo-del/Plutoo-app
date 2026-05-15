2d7c561df6a0fe22892dd7766da289b0b85e331e
mobricifilippo-del <mobrici.filippo@live.com>
2026-05-15 20:10:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5345,7 +5345,9 @@ fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${state.geo
       },
       
       (err) => {
-  createDogZoneInput.value = "GEO KO code " + (err && err.code ? err.code : "?");
+  createDogZoneInput.value = state.lang === "it"
+  ? "Posizione non rilevata"
+  : "Location not detected";
 },
 
       { enableHighAccuracy:true, timeout:10000, maximumAge:0 }
