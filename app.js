f83f72d3f245e5edabd0eaaae74061c444660581
mobricifilippo-del <mobrici.filippo@live.com>
2026-05-18 17:14:34 +0200

    Update app.js

--- app.js
+++ app.js
@@ -7131,17 +7131,6 @@ if (likeDogBtn) {
       createdAt: firebase.firestore.FieldValue.serverTimestamp()
     }, { merge: true });
 
-const likeNotifId = `like_${String(fromDogId)}_${String(toDogId)}`;
-
-await db.collection("notifications").doc(likeNotifId).set({
-  type: "like",
-  fromUid: String(fromUid),
-  fromDogId: String(fromDogId),
-  toDogId: String(toDogId),
-  createdAt: firebase.firestore.FieldValue.serverTimestamp(),
-  read: false
-}, { merge: true });
-
     const inverseLike = await db.collection("likes").doc(`${toDogId}_${fromDogId}`).get();
 
     if (inverseLike && inverseLike.exists) {
