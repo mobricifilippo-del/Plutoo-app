8af96163ba7b993a9b6a0b1ed0695a96faa3d9fc
mobricifilippo-del <mobrici.filippo@live.com>
2026-06-02 15:12:30 +0200

    Update app.js

--- app.js
+++ app.js
@@ -9501,7 +9501,6 @@ await window.db.collection("chats").doc(chatId).set({
   confirmText: "OK"
 });
       closeViewer();
-     openChat(chatId, dogId, otherUid);
 
     } catch (e) {
       console.error("DogBoard viewer send error:", e);
