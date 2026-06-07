3eb5e1cce70e41bfdc46d860938bad58f4e72aae
mobricifilippo-del <mobrici.filippo@live.com>
2026-06-07 15:51:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -8718,13 +8718,12 @@ if (existingDoc) {
 
     deleteStorage
       .then(() => {
-        
-    return window.db
-  .collection("dogsPrivate")
-  .doc(String(dogId))
-  .collection("dogDocs")
-  .doc(String(docName))
-  .delete();
+
+        return window.db.collection("dogs").doc(dogId).set({
+  dogDocs: {
+    [docName]: firebase.firestore.FieldValue.delete()
+  }
+}, { merge: true });
         
       })
       .catch((err) => { storageRef.delete().catch(() => {}); throw err; })
